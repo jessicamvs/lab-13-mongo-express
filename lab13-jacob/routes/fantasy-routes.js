1364618a141e7753f4a981a16dc395992164044e
jisenber <isenberg.jacob@yahoo.com>
2016-12-24 21:14:16 -0800

    error handling for routes finished, some refactoring, moving to tests

--- lab13-jacob/routes/fantasy-routes.js
+++ lab13-jacob/routes/fantasy-routes.js
@@ -5,6 +5,11 @@ let League = require('../model/fantasy');
 let router = module.exports = new Router();
 
 router.post('/leagues', jsonParser, (req, res, next) => {
+  if (Object.keys(req.body).length === 0 || !req.body.leagueName) {
+    res.status(400).end('bad request');
+    return;
+  }
+  res.status(200).write('creating new league...');
   new League(req.body).save()
   .then(league => res.json(league))
   .catch(next);
@@ -12,34 +17,58 @@ router.post('/leagues', jsonParser, (req, res, next) => {
 //when the router encounters a post request, parse the body, take the request, response and next operations. Make a new League with the body, then save it.
 
 router.get('/leagues/:id', (req, res, next) => {
-  League.findById(req.params.id)//database read
-  .then(league => res.json(league))
+  League.findById(req.params.id, function(err) {
+    if(err) {
+      res.status(404).end('ID not found');
+    }
+  })//database read
+  .then(league => res.status(200).json(league))
   .catch(next);
 });
 
 router.put('/leagues/:id', jsonParser, (req, res, next) => {
-  League.findByIdAndUpdate(req.params.id,
-    {leagueName: req.body.leagueName}, err => {
+  League.findById(req.params.id, function(err) {
       //     // {numPlayers: req.body.numPlayers},
       //     // {dateOfCreation: req.body.dateOfCreation},
-      //     // {commissioner: req.body.commissioner}]);
-      if(err) {
+      //     // {commissioner: req.body.commissioner}]); //am I feeling ambituous?? not yet...
+    if (err) {
+      if (Object.keys(req.body).length === 0 || !req.body.leagueName) {
+        res.status(400).end('bad request');
+        return;
+      } else {
+        res.status(404).end('not found');
+        return;
+      }
+    }
+  })
+  .then(function(league) {
+    league.update({leagueName: req.body.leagueName}, function(err) {
+      if (err) {
         console.error(err);
       }
+      res.status(200).end('League successfully updated');
     });
-  res.end('League successfully updated');
-  next();
+  })
+  .catch(next);
 });
+// });
 
-router.delete('/leagues/:id', (req, res) => {
-  League.findByIdAndRemove(req.params.id, err => {
+router.delete('/leagues/:id', (req, res, next) => {
+  League.findById(req.params.id, err => {
     if(err) {
-      console.error(err);
+      res.status(404).end('not found');
+      return;
     }
-  });
-  res.end('fantasy league succesfully deleted');
+  })
+  .then(function(league) {
+    league.remove({leagueName: league.leagueName}, function(err) {
+      if(err) {
+        console.error(err);
+        res.status(404).end('not found');
+        return;
+      }
+      res.status(204).end();
+    });
+  })
+  .catch(next);
 });
-
-
-
-//mongodb://localhost/usr/local/var/mongodb
--- lab13-jacob/test/fantasy-test.js
+++ lab13-jacob/test/fantasy-test.js
@@ -1,4 +1,6 @@
 let expect = require('chai').expect;
+let request = require('superagent').request;
+let testLeague = '';
 
 describe('our server', function() {
   let server = undefined;
@@ -6,7 +8,39 @@ describe('our server', function() {
     server = require('../index.js');
     server.lisen(3000);
   });
-  //TODO: DO ALL MY TESTS GO HERE
+  describe('my fantasy football API that uses mongoDB to store league information', function() {
+    describe('the POST route', function() {
+      it('should post an instance of a league with a leaguename property if given a valid input', function(done) {
+        request.post('http://localhost:3000/leagues')
+        .send({leagueName: 'Test League'})
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.leagueName).to.equal("Test League");
+          testLeague = res.body;
+          done();
+        });
+      });
+      it('should return a \'bad request\' error message if no body is provided in the request', function(done) {
+        request.post('http://localhost:3000/leagues')
+        .send({})
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.text).to.equal('bad request');
+          done();
+        });
+      });
+      it('should return a \'bad request\' error message if an invalid body is provided in the request', function(done) {
+        request.post('http://localhost:3000/leagues')
+        .send({cheese: 'gouda'})
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.text).to.equal('bad request');
+          done()
+        })
+      })
+    });
+  });
 
   after(function() {
     server.close();
