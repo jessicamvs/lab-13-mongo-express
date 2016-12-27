dad658b054d95084a6e63ae055dd8f7ddd8b28f7
Geoffrey Cox <gcox3d@gmail.com>
2016-12-27 15:52:24 -0800

    removed a few comments

--- lab-geoff/server.js
+++ lab-geoff/server.js
@@ -2,12 +2,10 @@
 
 let express = require('express');
 let mongoose = require('mongoose');
-// let jsonParser = require('body-parser').jsonParser; //not sure
 
 // let MONGO_URI = process.env.MONGO_URI;
 let MONGO_URI = 'mongodb://localhost/albums';
 console.log(MONGO_URI);
-let PORT = process.env.PORT || 3000;
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URI);
 
@@ -15,10 +13,9 @@ let app = express();
 
 let albumRouter = require('./route/albumRoutes.js');
 app.use(albumRouter);
-// app.use('/albums', albumRouter);
 
 module.exports = app;
-//
+
 // app.listen(PORT, () => {
 //   console.log(`Server on ${PORT}`);
 // });
