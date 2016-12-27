e5b5ee41c6e11978d2fc7495228f979333f4df5f
Geoffrey Cox <gcox3d@gmail.com>
2016-12-26 19:29:53 -0800

    added body-parser and required routes

--- lab-geoff/server.js
+++ lab-geoff/server.js
@@ -1,8 +1,15 @@
 'use strict';
 
 let express = require('express');
+let jsonParser = require('body-parser').json;
 
 let app = express();
+let router = express.Router();
+
+app.use(jsonParser);
+require('./route/albumRoutes.js')(router);
+app.use(router);
+
 let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
