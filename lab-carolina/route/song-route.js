3ae6ede9e36c11b46399d373d4f7fcde0dad9510
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-12-27 17:11:30 -0800

    put delete and tests passing

--- lab-carolina/route/song-route.js
+++ lab-carolina/route/song-route.js
@@ -2,6 +2,7 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
 const Song = require('../model/song.js');
 
 const songRouter = module.exports = new Router();
@@ -9,11 +10,23 @@ const songRouter = module.exports = new Router();
 songRouter.post('/api/song', jsonParser, function(req, res, next){
   new Song(req.body).save()
   .then(song => res.json(song))
-  .catch(next);
+  .catch(err => next(createError(400, err.message)));
 });
 
 songRouter.get('/api/song/:id', function(req, res, next){
   Song.findById(req.params.id)
   .then(song => res.json(song))
-  .catch(next);
+  .catch(err => next(createError(404, err.message)));
+});
+
+songRouter.put('/api/song/:id', jsonParser, function(req, res, next){
+  Song.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(song => res.json(song))
+  .catch(err => next(createError(400, err.message)));
+});
+
+songRouter.delete('/api/song/:id', function(req, res, next){
+  Song.findByIdAndRemove(req.params.id)
+  .then(() => res.sendStatus(204))
+  .catch(err => next(createError(404, err.message)));
 });
