c8f6098bcf37b03a661f9e0c25f0456671876686
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-12-28 17:54:39 -0800

    Only 4 tests passing

--- lab-jessica/index.js
+++ lab-jessica/index.js
@@ -3,8 +3,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jsonParser = require('body-parser').json();
+const morgan = require('morgan');
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = 'mongodb://jessica:yes@ds145178.mlab.com:45178/jessicas-books';
 console.log(MONGODB_URI);
 const PORT = process.env.PORT || 3000;
 
@@ -13,11 +14,12 @@ mongoose.connect(MONGODB_URI);
 
 const app = express();
 const bookRouter = require('./routes/book-routes.js');
+app.use(morgan('dev'));
 app.use(jsonParser);
 app.use('/api', bookRouter);
 
-// module.exports = app;
+module.exports = app;
 
-// if (require.main === module) {
-app.listen(PORT, () => console.log(`server started on ${PORT}`));
-// }
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`server started on ${PORT}`));
+}
--- lab-jessica/routes/book-routes.js
+++ lab-jessica/routes/book-routes.js
@@ -5,20 +5,15 @@ const Router = require('express').Router;
 const Book = require('../models/book.js');
 const router = module.exports = new Router();
 
-// ### `/api/model-name`
-// * `POST` request
 //  * pass data as stringified json in the body of a post request to create a resource
 router.post('/books', (req, res, next) => {
   new Book(req.body).save()
     .then(book => {
-      console.log(book);
       res.json(book);
     })
     .catch(next);
 });
 
-// ### `/api/model-name/:id`
-// * `GET` request
 //  * pass the id of a resource though the url endpoint to `req.params` to fetch a model
 router.get('/books/:id', (req, res, next) => {
   Book.findById(req.params.id)
@@ -26,15 +21,13 @@ router.get('/books/:id', (req, res, next) => {
     .catch(next);
 });
 
-// * `PUT` request
 //  * pass data as stringified json in the body of a put request to update a resource
 router.put('/books/:id', (req, res, next) => {
-  Book.findOneAndUpdate({_id: req.params.id}, req.body)
+  Book.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
     .then(book => res.json(book))
     .catch(next);
 });
 
-// * `DELETE` request
 //  * pass the id of a resource though the url endpoint to `req.params` to delete a model
 router.delete('/books/:id', (req, res, next) => {
   Book.remove({_id: req.params.id})
